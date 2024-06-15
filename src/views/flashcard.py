8f1a9209eac8bcc10f893731efc2a1588f399da3
Shepherd Smith <appliedchristian@gmail.com>
2024-06-14 22:31:04 -0400

    add parsing bar

--- src/views/flashcard.py
+++ src/views/flashcard.py
@@ -47,7 +47,6 @@ class FlashCardContainer(ui.card):
 
         with self: # default for front
             getFlashCardElement(self.flashcard, self._side, self.class_str)
-            getFlashCardParsing(self.flashcard)
 
 
     def toggle(self) -> None:
@@ -59,9 +58,7 @@ class FlashCardContainer(ui.card):
         self.clear()
         with self:
             c = getFlashCardElement(self.flashcard, self._side, self.class_str)
-            p = getFlashCardParsing(self.flashcard)
             c.update()
-            p.update()
 
         self.update()
 
@@ -73,32 +70,101 @@ class ViewFlashCard():
     def __init__(self):
         self.setupView()
     
-    def load_flashcards(self):
-        fset = FlashCardSet("1 John 1")
-        f_cards = fset.get_flashcard_set_verse_words(verse_num=1)
+    def load_parsed_icons(self, f:FlashCard):
+        if(f.tense != None):
+            match f.tense:
+                case Tense.PRESENT:
+                    ui.image("src/images/icons/tense_present.png").classes("w-6 h-6")
+                case Tense.IMPERFECT:
+                    ui.image("src/images/icons/tense_imperfect.png").classes("w-6 h-6")
+                case Tense.FUTURE:
+                    ui.image("src/images/icons/tense_future.png").classes("w-6 h-6")
+                case Tense.AORIST:
+                    ui.image("src/images/icons/tense_aorist.png").classes("w-6 h-10")
+                case Tense.FIRST_AORIST:
+                    ui.image("src/images/icons/tense_aorist.png").classes("w-6 h-10")
+                case Tense.SECOND_AORIST:
+                    ui.image("src/images/icons/tense_aorist.png").classes("w-6 h-10")
+                case Tense.PERFECT:
+                    ui.image("src/images/icons/tense_perfect.png").classes("w-6 h-6")
+                case Tense.PLUPERFECT:
+                    ui.image("src/images/icons/tense_pluperfect.png").classes("w-6 h-6")
+        if(f.voice != None):
+            match f.voice:
+                case Voice.ACTIVE:
+                    ui.image("src/images/icons/voice_active.png").classes("w-7 h-6")
+                case Voice.MIDDLE_PASSIVE:
+                    ui.image("src/images/icons/voice_middle_passive.png").classes("w-7 h-10")
+        if(f.mood != None):
+            match f.mood:
+                case Mood.INDICATIVE:
+                    ui.label(" ")
+                case Mood.IMPERATIVE:
+                    ui.label("!")
+                case Mood.SUBJUNCTIVE:
+                    ui.label("?")
+                case Mood.OPTATIVE:
+                    ui.label("??")
+                case Mood.PARTICIPLE:
+                    ui.label("P")
+                case Mood.INFINITIVE:
+                    ui.label("I")
+        if(f.person != None):
+            match f.person:
+                case Person.FIRST:
+                    ui.label("1")
+                case Person.SECOND:
+                    ui.label("2")
+                case Person.THIRD:
+                    ui.label("3")
+        if(f.case != None):
+            match f.case:
+                case Case.NOMINATIVE:
+                    ui.label("N")
+                case Case.GENITIVE:
+                    ui.label("G")
+                case Case.DATIVE:
+                    ui.label("D")
+                case Case.ACCUSATIVE:
+                    ui.label("A")
+        if(f.gender != None):
+            match f.gender:
+                case Gender.FEMININE:
+                    ui.image("src/images/icons/gender_feminine.png").classes("w-6 h-6")
+                case Gender.MASCULINE:
+                    ui.image("src/images/icons/gender_masculine.png").classes("w-6 h-6")
+                case Gender.NEUTER:
+                    ui.image("src/images/icons/gender_neuter.png").classes("w-6 h-6")
+        if(f.number != None):
+            match f.number:
+                case Number.PLURAL:
+                    ui.image("src/images/icons/number_singular.png").classes("w-6 h-6")
+                case Number.SINGULAR:
+                    ui.image("src/images/icons/number_plural.png").classes("w-8 h-6")
 
+    def load_flash_cards(self, f_cards:list):
         for f in f_cards[:100]:
             card_class_str = "items-center"
-            if(f.gender != None):
-                match f.gender:
-                    case Gender.FEMININE:
-                        card_class_str += " border-red-700"
-                    case Gender.MASCULINE:
-                        card_class_str += " border-blue-700"
-                    case Gender.NEUTER:
-                        card_class_str += " border-black-700"
-                card_class_str += " border-2 border-solid"
-            
-            # match f.number:
-            #     case Number.PLURAL:
-            #         card_class_str += " shadow-lg shadow-orange-900"
-            #     case Number.SINGULAR:
-            #         card_class_str += " shadow-none"
-            #     case None:
-            #         card_class_str += " shadow-none"
-
             with ui.card().classes(card_class_str):
                 FlashCardContainer(flashcard=f, class_str="w-24 h-24 text-center")
+                # Now put the parsing info below the image
+                with ui.row().classes("items-center"):
+                    self.load_parsed_icons(f)
+    
+
+    def load_flashcard_view(self):
+        chapter_ref = "1 John 1"
+        fset = FlashCardSet(chapter_ref)
+        ui.label(chapter_ref)
+
+        # ui.number(label='Verse', value=1,
+        #   on_change=lambda e: self.update_flashcards(e.value))
+
+        f_cards = fset.get_flashcard_set_verse_words(verse_num=1)
+        self.load_flash_cards(f_cards)
+
+        
+                    
 
 
     def setupView(self):
@@ -107,7 +173,7 @@ class ViewFlashCard():
             with navbar('Session In Progress'):
                 ### Header ###
                 with ui.row().classes('w-full items-center') as content:
-                    self.load_flashcards()
+                    self.load_flashcard_view()
 
                 
                             
