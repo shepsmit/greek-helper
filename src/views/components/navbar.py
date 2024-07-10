4c005bab74395ee9d57971939cf79cc89ffb35bb
Shepherd Smith <appliedchristian@gmail.com>
2024-07-09 22:11:37 -0400

    navbar tweaks for mobile

--- src/views/components/navbar.py
+++ src/views/components/navbar.py
@@ -6,12 +6,9 @@ from nicegui import ui
 def navbar(navigation_title: str):
     """Custom page frame to share the same styling and behavior across all pages"""
     ui.colors(primary='#48bf53', secondary='#11823b', accent='#91f086', positive='#53B689')
-    with ui.header():
-        with ui.button(icon='menu'):
-            with ui.menu() as menu:
-                ui.menu_item('Placeholder', lambda: ui.notify("Hi"))
-        ui.button("Flashcards", on_click=lambda: ui.navigate.to("/"))
-        ui.button("Reference", on_click=lambda: ui.navigate.to("/reference"))
+    with ui.header().props('reveal'):
+        ui.button("Cards", on_click=lambda: ui.navigate.to("/"))
+        ui.button("Ref", on_click=lambda: ui.navigate.to("/reference"))
         ui.button("Songs", on_click=lambda: ui.navigate.to("/songs"))
     with ui.column().classes('w-full items-center'):
         yield
